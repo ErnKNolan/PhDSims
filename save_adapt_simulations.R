64bc90e5ea83b5b14e54eae60917ee2520cf33be
Erin <111838888+ErnNolan@users.noreply.github.com>
2024-11-14 15:50:47 +1100

    lower n_per_k no interim saved

--- save_adapt_simulations.R
+++ save_adapt_simulations.R
@@ -11,8 +11,8 @@ properties <- readRDS(here("Data","properties.RDS"))
 
 #Take out the trial properties
 trial_props <- list()
-for(j in 1:60){
-  for(i in seq(2,7500,2)){ #the middle number is the total number of items in the list
+for(j in 1:36){
+  for(i in seq(2,5000,2)){ #the middle number is the total number of items in the list
     trial_props[[length(trial_props)+1]] <- test[[j]][[i]]
     trial_props[[length(trial_props)]]$sim <- i/2 #label each simulation
     trial_props[[length(trial_props)]]$property <- j #label each property combination
@@ -21,24 +21,10 @@ for(j in 1:60){
 trial_props <- bind_rows(trial_props)
 #saveRDS(trial_props,here("Data","adapt_trial_props.RDS"))
 
-#Take out the interim analyses
-interim <- list()
-for(j in 1:60){
-  for(i in seq(1,7500,3)){ #the middle number is the total number of items in the list
-    interim[[length(interim)+1]] <- test[[j]][[i]]
-    interim[[length(interim)]]$sim <- (i+2)/3 #label each simulation
-    interim[[length(interim)]]$property <- j #label each property combination
-  }
-}
-interim <- bind_rows(interim)
-properties2 <- properties %>% mutate(row = row_number()) 
-interim2 <- merge(interim,properties2,by.y=c("row"),by.x="property")
-#saveRDS(interim2,here("Data","adapt_interim.RDS"))
-
 #Take out the full analyses
 tempd <- list()
-for(j in c(1:60)){
-  for(i in seq(1,7500,2)){ #the middle number is the total number of items in the list
+for(j in c(1:36)){
+  for(i in seq(1,5000,2)){ #the middle number is the total number of items in the list
     tempd[[length(tempd)+1]] <- test[[j]][[i]]
     tempd[[length(tempd)]]$sim <- (i+1)/2 #label each simulation
     tempd[[length(tempd)]]$property <- j #label each property combination
