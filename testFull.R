f52e451c1dba41375c7a7353df9f467d625880b3
Erin <111838888+ErnNolan@users.noreply.github.com>
2024-11-14 15:51:38 +1100

    more draws and add prob direction

--- testFull.R
+++ testFull.R
@@ -48,8 +48,8 @@ testFull <- function(t,expdat,rho,mod,outdir,int_dat,...){
     res <- mod$sample(
       data = data, 
       init = 0,
-      iter_warmup = 500,
-      iter_sampling = 500,
+      iter_warmup = 750,
+      iter_sampling = 750,
       chains = 4, 
       parallel_chains = 1,
       adapt_delta = 0.99,
@@ -61,7 +61,7 @@ testFull <- function(t,expdat,rho,mod,outdir,int_dat,...){
     print(j)
     time <- toc()
     time <- time$toc - time$tic
-    results <- list(data.frame(res$summary(variables=c("pred_prob_trt","beta_trt","pp_trt2","pp_trt3","pp_trt4")),time=time))
+    results <- list(data.frame(res$summary(variables=c("pred_prob_trt","beta_trt","pp_trt2","pp_trt3","pp_trt4","probd_trt2","probd_trt3","probd_trt4")),time=time))
   },
   
   error=function(e) { message(conditionMessage(e)) 
